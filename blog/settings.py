f8d4354db1538d959e00cfcc74bb6d34755e7b7c
Arber Biljali <119668272+arberb96@users.noreply.github.com>
2024-05-29 18:30:28 +0200

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-3x!!gd%&%dy7)%l0p()#+w2h$7*l@s6c22zhqi$d^fbaomgp6s"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*", "arbertest.pythonanywhere.com"]
 
